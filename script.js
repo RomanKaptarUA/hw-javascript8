42439a75a5f5867a20349070d2a330b86d99d9e0
RomanKaptarUA <romankaptar82@gmail.com>
2024-06-12 19:11:24 +0300

    .

---
+++ script.js
@@ -0,0 +1,15 @@
+// 3
+const cards = [
+    'Карточка-1',
+    'Карточка-2',
+    'Карточка-3',
+    'Карточка-4',
+    'Карточка-5',
+];
+const cardToRemove = cards.splice(0, 3);
+console.log(cards);
+console.log(cardToRemove);
+const cardToInsert = (5, 1, 'Карточка-6');
+console.log(cardToInsert);
+const cardToUpdate = (3, 1, 'Карточка-4')
+
